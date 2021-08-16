c3ec0dba5fa96c4b42ad8a6449f50c0a80659dc4
YutakaAihara <youguisufanyuan@gmail.com>
2021-08-16 13:55:36 +0900

    変数・定数

---
+++ src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String str1 = "YutakaAihara";
+		System.out.println(str1);
+		str1 = "Javaがんばるど";
+		System.out.println(str1);
+		final int LUCKY_NUMBER = 107;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
